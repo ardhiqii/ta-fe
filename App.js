3b7b2cac3e39bde2339b91801c94c45498627574
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2024-02-18 14:40:21 +0700

    init font lexend

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {useFonts,LexendDeca_400Regular} from '@expo-google-fonts/lexend-deca';
 export default function App() {
+  let[fontsloaded]= useFonts({
+    LexendDeca_400Regular,
+  })
+  if(!fontsloaded){
+    return <View><Text>WAITING FONT</Text></View>
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontFamily:"LexendDeca_400Regular"
+  }
 });
